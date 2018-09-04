8a94abe964ec5e698403d2f2b40253a7cc0557a3
anton-sales <anton.arus@gmail.com>
2018-09-05 09:14:39 +1200

    added more test

--- src/components/Square/index.spec.js
+++ src/components/Square/index.spec.js
@@ -12,13 +12,13 @@ describe('components:Square', () => {
 
   it('renders the Square with the proper styles for player X in the top left square', () => {
     expect(
-      toJson(shallow(<Square player='x' index={0} />).dive())
+      toJson(shallow(<Square player='o' index={0} />).dive())
     ).toMatchSnapshot()
   })
 
   it('renders the Square with the proper styles for player X in the top right square', () => {
     expect(
-      toJson(shallow(<Square player='x' index={2} />).dive())
+      toJson(shallow(<Square player='o' index={2} />).dive())
     ).toMatchSnapshot()
   })
 
@@ -37,6 +37,24 @@ describe('components:Square', () => {
   it('renders the Square with the proper styles for player O in the bottom middle square', () => {
     expect(
       toJson(shallow(<Square player='o' index={7} />).dive())
+    ).toMatchSnapshot()
+  })
+
+  it('renders the Square with the proper styles for player O in the top middle square', () => {
+    expect(
+      toJson(shallow(<Square player='o' index={1} />).dive())
+    ).toMatchSnapshot()
+  })
+
+  it('renders the Square with the proper styles for player X in the middle left square', () => {
+    expect(
+      toJson(shallow(<Square player='o' index={3} />).dive())
+    ).toMatchSnapshot()
+  })
+
+  it('renders the Square with the proper styles for player X in the middle center square', () => {
+    expect(
+      toJson(shallow(<Square player='x' index={4} />).dive())
     ).toMatchSnapshot()
   })
 })
